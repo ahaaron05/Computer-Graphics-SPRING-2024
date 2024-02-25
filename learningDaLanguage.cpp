944440565346133e7b949fad86d150f82a7c5f2e
Aaron Ludwig <aaronludwig123@gmail.com>
2024-02-25 17:21:08 -0600

    still learning...

--- learningDaLanguage.cpp
+++ learningDaLanguage.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 
+float gallons[5] = {15.75f, 16.3f, 10.95f, 20.65f, 30.0f};
+float totalGalls = 0;
 
-////////////////////
-/////////////
-// MAIN
+
+float AvgMilePerGall(int starting, int ending)
+{
+	for (int i = 0; i < sizeof(gallons); i++)
+		totalGalls += gallons[i];
+
+	return (ending - starting) / totalGalls;
+}
+
+
 int main()
 {
-	std::cout << "Hello world\n";
-
-	return 0;
+	std::cout << AvgMilePerGall(68723, 71289);
 }
