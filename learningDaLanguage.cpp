ad529063640b0f9f498896f676a11510fc5a5774
Aaron Ludwig <107589333+ahaaron05@users.noreply.github.com>
2024-02-26 09:17:43 -0600

    still learning...

--- learningDaLanguage.cpp
+++ learningDaLanguage.cpp
@@ -1,19 +1,47 @@
 #include <iostream>
 
-float gallons[5] = {15.75f, 16.3f, 10.95f, 20.65f, 30.0f};
-float totalGalls = 0;
+using namespace std;
 
 
-float AvgMilePerGall(int starting, int ending)
+float CalculateAvg(float s1, float s2, float s3, float s4)
 {
-	for (int i = 0; i < sizeof(gallons); i++)
-		totalGalls += gallons[i];
-
-	return (ending - starting) / totalGalls;
+	return (s1 + s2 + s3 + s4) / 4;
 }
 
+float CalculateHighest(float s1, float s2, float s3, float s4)
+{
+	return max({ s1, s2, s3, s4 });
+}
+
+void PrintBelow(float avg, float s1, float s2, float s3, float s4)
+{
+	if (s1 < avg)
+		cout << "Bill\n";
+	else if (s2 < avg)
+		cout << "Tom\n";
+	else if (s3 < avg)
+		cout << "Joe\n";
+	else if (s4 < avg)
+		cout << "Sarah\n";
+}
+
+void PrintHighest(float highest, float s1, float s2, float s3, float s4)
+{
+	if (s1 == highest)
+		cout << "Bill\n";
+	else if (s2 == highest)
+		cout << "Tom\n";
+	else if (s3 == highest)
+		cout << "Joe\n";
+	else if (s4 == highest)
+		cout << "Sarah\n";
+}
 
 int main()
 {
-	std::cout << AvgMilePerGall(68723, 71289);
+	float avg = CalculateAvg(95, 22, 87, 90);
+	float highest = CalculateHighest(95, 22, 87, 90);
+
+	PrintBelow(avg, 95, 22, 87, 90);
+	PrintHighest(highest, 95, 22, 87, 90);
 }
