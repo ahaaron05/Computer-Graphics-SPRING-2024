b74bc86f69f77867abdb59447d98c0c0f988f021
Aaron Ludwig <107589333+ahaaron05@users.noreply.github.com>
2024-02-27 07:50:40 -0600

    still learning....

--- learningDaLanguage.cpp
+++ learningDaLanguage.cpp
@@ -7,5 +7,15 @@ void TagMaker(string n, int m, int d)
 {
 	cout << "***********************\n";
 	cout << "*\t Programming Assignment 1\t *\n";
+	cout << "*\t Computer Programming 1\t *\n";
+	cout << "*\t Author: " + n + "\t *\n";
+	cout << "*\t Date: " + m + '/' + d + '\t' + '*\n';
+	cout << "***********************\n";
+}
 
+int main()
+{
+	TagMaker("Aaron", 10, 13);
+
+	return 0;
 }
